1a07f7da05b82bbad43e761e9cd7a70e6c701d6d
Robert Olivas <robertolivas@Roberts-MBP-2.lan1>
2024-07-21 14:19:36 -0500

    completed project task

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: " https://cnt-dabceff9-3ab8-405e-b001-0470c3e677fd.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-573f96bc-ca38-4850-ac78-81a57943ace2.containerhub.tripleten-services.com"
 }
